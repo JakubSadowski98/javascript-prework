7d0cf4d9f85f39d207810071df4ca460529ab94a
Jakub Sadowski <kuba.sadowsky@gmail.com>
2024-01-09 16:17:31 +0100

    Add new code in index.html functions.js script.js

--- js/functions.js
+++ js/functions.js
@@ -1,9 +1,42 @@
-function printMessage(msg){
+var argMoveId, argPlayerMove, argComputerMove;
+
+function printMessage(msg) {
 	var div = document.createElement('div');
 	div.innerHTML = msg;
 	document.getElementById('messages').appendChild(div);
 }
 
-function clearMessages(){
+function clearMessages() {
 	document.getElementById('messages').innerHTML = '';
 }
+
+function getMoveName(argMoveId) {
+  console.log('wywołano funkcję getMoveName z argumentem: ' + argMoveId);
+  if (argMoveId == 1) {
+    return 'kamień';
+  } else if (argMoveId == 2) {
+    return 'papier';
+  } else if (argMoveId == 3) {
+	return 'nożyce';
+  } else {
+	printMessage('Nie znam ruchu o id = ' + argMoveId + ' .Zakładam, że chodziło o "kamień".');
+	return 'kamień';
+  }
+}
+
+function displayResult(argPlayerMove, argComputerMove) {
+  console.log('wywołano funkcję displayResults z argumentami: ' + argPlayerMove + ', ' + argComputerMove);
+  if (argPlayerMove == 'kamień' && argComputerMove == 'nożyce') {
+    printMessage('Wygrywasz!');
+  } else if (argPlayerMove == 'papier' && argComputerMove == 'kamień') {
+	printMessage('Wygrywasz!');
+  } else if (argPlayerMove == 'nożyce' && argComputerMove == 'papier') {
+	printMessage('Wygrywasz!');
+  } else if (argPlayerMove == argComputerMove) {
+	printMessage('Remis');
+  } else {
+    printMessage('Przegrywasz :(');
+  }
+  printMessage('Zagrałem ' + argComputerMove + ', a Ty ' + argPlayerMove);
+}
+
--- js/script.js
+++ js/script.js
@@ -1,3 +1,32 @@
-var computerMove = 'kamień';
-var playerMove ='papier';
-printMessage('Zagrałem' + computerMove + '!Jeśli Twój ruch to' + playerMove + ', to wygrywasz!');
+var buttonRock, buttonPaper, buttonScissors; // tu będą wymienione pozostałe zmienne guzików, np. buttonRock
+
+buttonRock = document.getElementById('button-rock'); // tu będą kolejne linie kodu, w których do zmiennych będą przypisane elementy znalezione po id
+buttonPaper = document.getElementById('button-paper');
+buttonScissors = document.getElementById('button-scissors');
+
+var argButtonName;
+
+function buttonClicked(argButtonName){ //początek deklaracji funkcji
+    clearMessages();
+    console.log(argButtonName + ' został kliknięty');
+
+    var computerMove, playerMove, randomNumber;
+
+    playerMove = argButtonName;
+    console.log('ruch gracza to: ' + playerMove);
+    randomNumber = Math.floor(Math.random() * 3 + 1);
+    console.log('wylosowana liczba to: ' + randomNumber);
+    computerMove = getMoveName(randomNumber);
+    console.log('ruch komputera to: ' + computerMove);
+    displayResult(playerMove, computerMove);
+}                                       //koniec deklaracji funkcji
+
+buttonRock.addEventListener('click', function(){ // tu będą kolejne powiązania guzików z funkcją buttonClicked
+    buttonClicked('kamień')
+});
+buttonPaper.addEventListener('click', function(){
+    buttonClicked('papier')
+});
+buttonScissors.addEventListener('click', function(){
+    buttonClicked('nożyce')
+});
